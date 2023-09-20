930df0ff42b70b8b454cbaa5027d12484a401b3e
OneTwo345 <codeevil305@gmail.com>
2023-09-20 14:45:42 +0700

    xong dang ki cuoc hen

---
+++ src/main/java/com/example/spa_case/service/comboService/ComboListResponse.java
@@ -0,0 +1,16 @@
+package com.example.spa_case.service.comboService;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+public class ComboListResponse {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
--- src/main/resources/myjs/productClient.js
+++ src/main/resources/myjs/productClient.js
@@ -13,40 +13,6 @@ async function renderCarousel() {
     renderServiceCarousel(list);
 }
 
-// function renderServiceCarousel(items) {
-//     let str = '';
-//     items.forEach((e, i) => {
-//         str += renderItemStr(e, i);
-//     })
-//     service.innerHTML = str;
-//
-//     // Service carousel
-//     $(".service-carousel").owlCarousel({
-//         autoplay: true,
-//         smartSpeed: 1500,
-//         loop: true,
-//         dots: false,
-//         nav : false,
-//         responsive: {
-//             0:{
-//                 items:1
-//             },
-//             576:{
-//                 items:2
-//             },
-//             768:{
-//                 items:3
-//             },
-//             992:{
-//                 items:4
-//             },
-//             1200:{
-//                 items:5
-//             }
-//         }
-//
-//     });
-// }
 function renderServiceCarousel(items) {
     let str = '';
     items.forEach((e, i) => {
