45bcae74a3616a3c589e76208e6a2a662d5b3597
OneTwo345 <codeevil305@gmail.com>
2023-09-22 10:30:34 +0700

    Last one

--- src/main/resources/myjs/comboClient.js
+++ src/main/resources/myjs/comboClient.js
@@ -1,5 +1,5 @@
 const service = document.getElementById("comboShow");
-let productsClient = [];
+let comboClients = [];
 
 async function getProducts() {
     const res = await fetch('http://localhost:8080/api/combos/list');
@@ -9,7 +9,7 @@ async function getProducts() {
 
 async function renderCarousel() {
     const list = await getProducts();
-    productsClient = list
+    comboClients = list;
     renderServiceCarousel(list);
 }
 
@@ -20,23 +20,27 @@ function renderServiceCarousel(items) {
     });
     service.innerHTML = str;
 
+    items.forEach(item => {
+        renderProductItem(item);
+    })
+
     if (items.length > 1) {
-        $(".service-carousel").owlCarousel({
+        $(".pricing-carousel").owlCarousel({
             autoplay: true,
             smartSpeed: 1500,
+            margin: 30,
             loop: true,
             dots: false,
-            nav: false,
-            items: 5,
+            nav : false,
             responsive: {
-                0: {
-                    items: 1
+                0:{
+                    items:1
                 },
-                600: {
-                    items: 3
+                576:{
+                    items:1
                 },
-                1000: {
-                    items: 5
+                768:{
+                    items:2
                 }
             }
         });
@@ -44,22 +48,30 @@ function renderServiceCarousel(items) {
 }
 
 function renderItemStr(item, index) {
-    return `  <div class="bg-white">
-                                <div class="d-flex align-items-center justify-content-between border-bottom border-primary p-4">
-                                    <h1 class="display-4 mb-0">
-                                        <small class="align-top text-muted font-weight-medium" style="font-size: 22px; line-height: 45px;">$</small>49<small class="align-bottom text-muted font-weight-medium" style="font-size: 16px; line-height: 40px;">/Mo</small>
-                                    </h1>
-                                    <h5 class="text-primary text-uppercase m-0">${item.name}</h5>
-                                </div>
-                                <div class="p-4">
-                                    <p><i class="fa fa-check text-success mr-2"></i>Full Body Massage</p>
-<!--                                    <p><i class="fa fa-check text-success mr-2"></i>Deep Tissue Massage</p>-->
-<!--                                    <p><i class="fa fa-check text-success mr-2"></i>Hot Stone Massage</p>-->
-<!--                                    <p><i class="fa fa-check text-success mr-2"></i>Tissue Body Polish</p>-->
-<!--                                    <p><i class="fa fa-check text-success mr-2"></i>Foot & Nail Care</p>-->
-                                    <a href="" class="btn btn-primary my-2">Order Now</a>
-                                </div>
-                            </div>`
+    return `  <div class="bg-white"  >
+                <div class="d-flex align-items-center justify-content-between border-bottom border-primary p-4">
+                    <h1 class="display-4 mb-0" style="margin: auto; color: pink" >
+                         ${item.name}
+                    </h1>
+                </div>
+                <div>
+                  <h5 class="text-primary text-uppercase m-0" style="text-align: center">${item.price} VND</h5>
+               </div>
+                
+                <div class="p-4" id="productItem${item.id}">
+                
+                </div>
+            </div>`
+}
+
+function renderProductItem(item) {
+    let strId = "productItem" + item.id;
+    let showListProduct = document.getElementById(strId);
+    item.products.forEach(e => {
+        let ele = document.createElement("p")
+        ele.innerHTML = "<i class=\"fa fa-check text-success mr-2\"></i>" + e;
+        showListProduct.appendChild(ele);
+    })
 }
 
 window.onload = async () => {
